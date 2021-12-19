924f714e1559bdce3badcf6ea6cdbad0a48a1793
Mohammed Agha <83072153+aghsa97@users.noreply.github.com>
2021-12-19 21:08:03 +0100

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBFc6tND25A4tXPh9aG6ds-ZgHOJ1lJKn8",
-    authDomain: "instagram-test-9122e.firebaseapp.com",
-    projectId: "instagram-test-9122e",
-    storageBucket: "instagram-test-9122e.appspot.com",
-    messagingSenderId: "687855187116",
-    appId: "1:687855187116:web:3ce722ae0c4557e7a41b09"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 const firebase = Firebase.initializeApp(firebaseConfig);
